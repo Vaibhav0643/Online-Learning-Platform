24463c6385394e746f2503505816fa8766d2f6d1
Ujjwal Chaudhary <40447532+legend3321@users.noreply.github.com>
2024-02-10 09:18:47 +0530

    Added Login Route

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,20 @@
 import "./App.css";
 import Dashboard from "./Components/Dashboard";
 
+import Login from "./Components/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
 function App() {
   return (
     <div className="App">
---
+++ Frontend/src/Components/Login.jsx
@@ -0,0 +1,32 @@
+import "../Assets/Login.css";
+
+function Login() {
+  return (
+    <div class="login-form-container">
+      <form>
+        <h3>Sign in to your account.</h3>
+        <div className="input_heading">EMAIL</div>
+        <input type="email" placeholder="name@email.com" class="box" />
+        <div className="input_heading">PASSWORD</div>
+        <input type="password" placeholder="Enter your password" class="box" />
+        <div className="password_hint">
+          <button className="forgot">Forgot password?</button>
+        </div>
+
+        {/* <input type="submit" value="login"class="btn" /> */}
+
+        <button className="btn">
+          Login <span>&#x2192; </span>
+        </button>
+
+        <hr class="divider" />
+        <p>
+          {" "}
+          don't have an account <a href="#">Sign Up</a>{" "}
+        </p>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
