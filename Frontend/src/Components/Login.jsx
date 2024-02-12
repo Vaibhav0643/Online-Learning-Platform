23d30ee1c05a7e3b4426941ae7434bfc938e1cb0
Vicky <itsvaibhav0604@gmail.com>
2024-02-12 23:45:38 +0530

    Fixed Login page font-size issues

--- Frontend/src/Components/Login.jsx
+++ Frontend/src/Components/Login.jsx
@@ -49,44 +49,63 @@ function Login() {
     setBtnDisabled("");
   };
   return (
-    <div className="login-form-container">
-      <form onSubmit={handleSubmit}>
-        <h3>Sign in to your account.</h3>
-        <div className="input_heading">EMAIL</div>
-        <input
-          id="email"
-          type="email"
-          placeholder="name@email.com"
-          className="box"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <div className="input_heading">PASSWORD</div>
-        <input
-          id="password"
-          type="password"
-          placeholder="Enter your password"
-          className="box"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <div className="password_hint">
-          <a href="/Forgot" className="forgot">
-            Forgot password?
-          </a>
+    <div className='app_content'>
+      <div>
+        <div className="login-form-container">
+          <form onSubmit={handleSubmit}>
+
+
+            <div >
+              <h3>Sign in to your account.</h3>
+              <p className='header_description'>
+                < span >Open the Door to Infinite Learning Opportunities.
+                  <br />
+                  Join Us to Shape Your Future Today.
+                </span>
+              </p>
+              <hr className="divider" />
+            </div>
+
+            <div className="input_heading">EMAIL</div>
+            <input
+              id="email"
+              type="email"
+              placeholder="name@email.com"
+              className="box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <div className="input_heading">PASSWORD</div>
+            <input
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+              className="box"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <div className="password_hint">
+              <a href="/Forgot" className="forgot">
+                Forgot password?
+              </a>
+            </div>
+
+            <button className="btn" disabled={isBtnDisabled}>
+              Login <span>&#x2192; </span>
+            </button>
+
+            <hr className="divider" />
+            <p className='footer_description'>
+              {" "}
+              Don't have an account? <a href="/signup">Sign Up</a>{" "}
+            </p>
+          </form>
         </div>
 
-        <button className="btn" disabled={isBtnDisabled}>
-          Login <span>&#x2192; </span>
-        </button>
-
-        <hr className="divider" />
-        <p>
-          {" "}
-          don't have an account <a href="/signup">Sign Up</a>{" "}
-        </p>
-      </form>
-    </div>
+      </div >
+    </div >
   );
 }
 
