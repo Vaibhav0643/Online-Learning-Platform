e5fbfcadd478426bf631dc466bc0d2d3e58bd841
Vicky <itsvaibhav0604@gmail.com>
2024-02-13 00:13:01 +0530

    Fixed Forgot Page font-size Issue

--- Frontend/src/Components/Forgot.jsx
+++ Frontend/src/Components/Forgot.jsx
@@ -1,23 +1,36 @@
 import '../Assets/Forgot.css';
 
-
-
 function Forgot() {
 
 
     return (
 
-        <div className="login-form-container">
-            <form>
-                <h3>Reset your password.</h3>
-                <div className='input_heading'>EMAIL</div>
-                <input type="email" placeholder="name@email.com" className="box" />
-                <button className='btn'>Reset Password <span>&#x2192; </span></button>
-                <hr className="divider" />
-                <p>Back to <a href="/login">Login</a> </p>
-            </form>
-        </div>
+        <div className='app_content'>
+            <div>
 
+                <div>
+                    <div className="forgot-form-container">
+                        <form>
+                        <div >
+                            <h3>Reset your password.</h3>
+                            <p className='header_description'>
+                                < span >Enter the email address you used when you signed up for our platform.
+                                </span>
+                            </p>
+                        </div>
+
+
+                            <input type="email" placeholder="name@email.com" className="box" />
+                            <button className='btn'>Reset Password <span>&#x2192; </span></button>
+                            <hr className="divider" />
+                            <p>Back to <a href="/">Login</a> </p>
+                        </form>
+                    </div>
+
+                </div>
+
+            </div >
+        </div >
     )
 }
 
