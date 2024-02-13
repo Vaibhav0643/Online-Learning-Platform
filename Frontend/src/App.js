476601694a9972018caea08b83e7f9cd743e249a
Ankit Gupta <g.ankit2001@gmail.com>
2024-02-13 19:07:13 +0530

    Added Enrollment page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,12 +6,14 @@ import Forgot from "./Components/Forgot";
 import Register from "./Components/Register";
 import Logout from "./Components/Logout";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Enrollment from "./Components/Enrollment";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/Login" element={<Login />} />
+        <Route path="/Enrollment" element={<Enrollment />} />
         <Route path="/Logout" element={<Logout />} />
         <Route path="/Signup" element={<Register />} />
         <Route path="/Dashboard" element={<Dashboard />} />
---
+++ Frontend/src/Components/Enrollment.jsx
@@ -0,0 +1,69 @@
+import "../Assets/Enrollment.css";
+
+function Enrollment() {
+    return (
+      <div className='app_content'>
+        
+          <div className="login-form-container">
+            <form>
+              <div>
+              <h3>Enrollment Form</h3>
+                <p className='header_description'>
+                  < span >Enroll in our courses and get certified by the best in the industry. <br />
+                  </span>
+                </p>
+                <hr className="divider" />
+              </div>
+
+              <div className="input_heading">Name</div>
+              <input
+                type="text"
+                placeholder="Enter your full name"
+                className="box"  
+              />
+
+              <div className="input_heading">Email</div>
+              <input
+                type="email"
+                placeholder="name@email.com"
+                className="box"
+              />
+
+              <div className="input_heading">Phone Number</div>
+              <input
+                type="text"
+                placeholder="Enter your phone number"
+                className="box"
+              />
+              
+              <div className="input_heading">Date Of Birth</div>
+              <input
+                type="date"
+                placeholder="Enter your DOB"
+                className="box"
+              />
+
+              <div className="input_heading">College/Organization</div>
+              <input
+                type="text"
+                placeholder="Enter name of your college/organization"
+                className="box"
+              />
+
+              <button className="btn">
+                Enroll <span>&#x2192; </span>
+              </button>
+
+              <hr className="divider" />
+              <p className='footer_description'>
+                {" "}
+                Back to <a href="/courses">Courses</a>{" "}
+              </p>
+            </form>
+          </div>
+
+      </div>
+      );
+    }
+    
+export default Enrollment;
