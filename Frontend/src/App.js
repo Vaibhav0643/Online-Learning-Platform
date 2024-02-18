faab5c781674c501b3d550ccd3c09bcbc11a5dcd
rairamjee <110187607+rairamjee@users.noreply.github.com>
2024-02-19 02:21:52 +0530

    Added logout button &individual course page
    Added the functionality to the logout button in the header .

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -8,6 +8,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Enrollment from "./Routes/Enrollment";
 import Upload from "./Routes/Upload";
 import Courses from "./Routes/Courses";
+import IndividualCourse from "./Routes/IndividualCourse.js";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path="/Courses" element={<Courses />} />
         <Route path="/Upload" element={<Upload />} />
         <Route path="/" element={<Home />} />
+        <Route path="/IndividualCourse" element={<IndividualCourse />} />
         <Route path="/Forgot" element={<Forgot />} />
       </Routes>
     </BrowserRouter>
