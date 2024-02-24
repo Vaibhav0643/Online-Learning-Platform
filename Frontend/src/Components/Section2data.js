56c2553c8cd3528ac0ac21a720f5630ad42fbef6
Pranjal Kumar <pranjalkumar.dev@gmail.com>
2024-02-24 14:36:36 +0530

    Added increasing number effect on Landing page

--- Frontend/src/Components/Section2data.js
+++ Frontend/src/Components/Section2data.js
@@ -1,28 +1,28 @@
 const data = [
   {
     id: 1,
-    number: "10 + ",
+    number: 15,
     topic: "Industry Experts",
     content: "Experienced tutors and industry registered experts",
   },
 
   {
     id: 2,
-    number: "20 + ",
+    number: 50,
     topic: "Total Courses Available",
     content: "The most demanding skills in the industry",
   },
 
   {
     id: 3,
-    number: "25 + ",
+    number: 100,
     topic: "Enrolled Students",
     content: "Ever increasing community of aspiring students",
   },
 
   {
     id: 4,
-    number: "5 + ",
+    number: 10,
     topic: "Different Fields",
     content: "Top Cousres of the demanding Fields",
   },
