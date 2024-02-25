292867fc7a66b4f6ce97d6ae21a54c1796767b4b
Prasad.D <prasadvenki60@gmail.com>
2024-02-26 00:49:18 +0530

    Signup validation

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -5,12 +5,45 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
+function checkemail(userEmail){
+  var checkDomain = 0;
+    var checkAtSign = 0;
+    if(userEmail.indexOf('@')>-1){
+      checkAtSign = 1;
+    }
+    if(userEmail.indexOf(".com")>-1 || userEmail.indexOf(".in")>-1 || userEmail.indexOf(".org")>-1){
+      checkDomain = 1;
+    }
+
+    if(checkAtSign != 1 || checkDomain != 1){
+      return 0;
+    }
+    else{
+      return 1;
+    }
+}
+
 const createUser = async (req, res) => {
   try {
     const { userEmail, userPassword, userName } = req.body;
 
     if (!userEmail || !userPassword || !userName) {
-      return res.status(400).json({ error: "Missing required fields" });
+      if(!userName){
+        return res.status(400).json({error: "Username can't be empty"});
+      }
+      else if(!userEmail){
+        return res.status(400).json({error: "UserEmail Can't be empty"});
+      }
+      else if(!userPassword){
+        return res.status(400).json({error: "Password can't be empty"});
+      }
+      else{
+        return res.status(400).json({ error: "Missing required fields" });
+      }
+    }
+
+    if(!checkemail(userEmail)){
+      return res.status(400).json({error:"Use a valid email id"})
     }
 
     const existingUser = await pool.query(
