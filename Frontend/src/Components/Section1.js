9829164279539ed394a1991ea8f2c5cd5c414e80
Pranjal Kumar <pranjalkumar.dev@gmail.com>
2024-02-27 21:15:08 +0530

    Frontend ready for AddCourse

--- Frontend/src/Components/Section1.js
+++ Frontend/src/Components/Section1.js
@@ -16,7 +16,7 @@ function Section1()
         <div className="content1">
             <p className="heading-1">FUTURE OF</p>
             <p className="heading-2">Online <span>Education.</span></p>
-            <p className="heading-3">"Unlock the world of knowledge with a click. Dive into a sea of courses tailored just for you. Learn, grow, and excel at your own pace. Your future starts here!"</p>
+            <p className="heading-3 poppins">"Unlock the world of knowledge with a click. Dive into a sea of courses tailored just for you. Learn, grow, and excel at your own pace. Your future starts here!"</p>
             
             {!user && <button className="home-login-btn" onClick={goToRegister}>Register  {' >'}</button>
 }
