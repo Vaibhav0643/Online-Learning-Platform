e61fc807da7f558909d7771254386bfb42222852
Jitendra Dhakar <Jitendrad869601@gmail.com>
2024-02-28 11:01:49 +0530

    fix update user progress Api

--- Frontend/src/Routes/IndividualCourses.js
+++ Frontend/src/Routes/IndividualCourses.js
@@ -1,11 +1,21 @@
-import React, { useState ,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "../Assets/IndividualCourses.css";
 import Header from "../Routes/Header";
 import Footer from "../Routes/Footer";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import Cookies from "universal-cookie";
-import { Button, Toolbar, Typography, Avatar, Box, CircularProgress, Checkbox, Container, FormControlLabel } from "@mui/material";
+import {
+  Button,
+  Toolbar,
+  Typography,
+  Avatar,
+  Box,
+  CircularProgress,
+  Checkbox,
+  Container,
+  FormControlLabel,
+} from "@mui/material";
 import profile from "../Images/profile.png";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -13,47 +23,81 @@ import "react-toastify/dist/ReactToastify.css";
 function IndividualCourse() {
   const params = useParams();
   const [course, setCourse] = React.useState(null);
-  const [checkedVideos, setCheckedVideos] = React.useState({});
-  const [admin , setAdmin]=useState(false);
+  const [checkedVideos, setCheckedVideos] = React.useState([]);
+  const [admin, setAdmin] = useState(false);
+  const [progress, setProgress] = useState(0);
 
   const id = params.id;
 
   useEffect(() => {
     const cookies = new Cookies();
-  
-    const user = JSON.parse(localStorage.getItem("user") || "{}"); 
-    if(user && user.userEmail === "ADMIN@GMAIL.COM") {
+
+    const user = JSON.parse(localStorage.getItem("user") || "{}");
+    if (user && user.userEmail === "ADMIN@GMAIL.COM") {
       setAdmin(true);
     }
   }, [params.id]);
 
-
-  let progress;
-  if(!admin){
-    progress = Object.values(checkedVideos).filter(checked => checked).length / (course?.courseDetails.videos.length || 1) * 100;
-  }
+  // let progress;
+  // if(!admin){
+  //   progress = Object.values(checkedVideos).filter(checked => checked).length / (course?.courseDetails.videos.length || 1) * 100;
+  // }
 
   const handleCheckboxChange = (index) => {
-    setCheckedVideos(prevState => ({
+    setCheckedVideos((prevState) => ({
       ...prevState,
-      [index]: !prevState[index]
+      [index]: !prevState[index],
     }));
+    const cookies = new Cookies();
+
+    const user = JSON.parse(localStorage.getItem("user"));
+    console.table([
+      user.userId,
+      params.id,
+      index + 1,
+      course.courseDetails.videoCount,
+    ]);
+    axios
+      .post(
+        `https://online-learning-platform-r55m.onrender.com/api/v1/course/${
+          user.userId
+        }/${params.id}/${index + 1}/${
+          course.courseDetails.videoCount
+        }/updateUserProgress`,
+        {
+          headers: {
+            Authorization: "Bearer " + cookies.get("token"),
+          },
+        }
+      )
+      .then((res) => {
+        setProgress(res.data.progress);
+      });
   };
 
   React.useEffect(() => {
     const cookies = new Cookies();
     axios
-      .get(`https://online-learning-platform-r55m.onrender.com/api/v1/course/${params.id}/getCourseDetails`, {
-        headers: {
-          Authorization: "Bearer " + cookies.get("token"),
-        },
-      })
+      .get(
+        `https://online-learning-platform-r55m.onrender.com/api/v1/course/${params.id}/getCourseDetails`,
+        {
+          headers: {
+            Authorization: "Bearer " + cookies.get("token"),
+          },
+        }
+      )
       .then((res) => {
         setCourse(res.data);
+        setProgress(res.data.courseDetails.progress);
+        const arr = [];
+        for (let i = 0; i < res.data.courseDetails.videoCount; i++) {
+          arr.push(false);
+        }
+        setCheckedVideos(arr);
+        console.log(progress);
         console.log(res.data);
       });
   }, [params.id]);
-
 
   const enroll = () => {
     const cookies = new Cookies();
@@ -68,6 +112,7 @@ function IndividualCourse() {
         }
       )
       .then((res) => {
+        console.log(res.data);
         window.location.reload();
         toast.success("Successfully enrolled in the course!");
       })
@@ -84,7 +129,9 @@ function IndividualCourse() {
     return course.courseDetails.videos.map((video, index) => {
       return (
         <div key={index}>
-          <h2 style={{ color: "blue", marginBottom: "2rem" }}>{video.videoTitle}</h2>
+          <h2 style={{ color: "blue", marginBottom: "2rem" }}>
+            {video.videoTitle}
+          </h2>
           <iframe
             width="450"
             height="340"
@@ -95,22 +142,32 @@ function IndividualCourse() {
             allowfullscreen
             style={{
               marginBottom: "2rem",
-              marginLeft:"1rem",
-              marginRight:"1rem"
+              marginLeft: "1rem",
+              marginRight: "1rem",
             }}
-            className="course-video">
-          </iframe>
+            className="course-video"
+          ></iframe>
 
           <br />
-         {!admin && <FormControlLabel control={<Checkbox checked={checkedVideos[index] || false} onChange={() => handleCheckboxChange(index)} />} label="Complete" />}
+          {!admin && (
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={checkedVideos[index] || false}
+                  onChange={() => handleCheckboxChange(index)}
+                />
+              }
+              label="Complete"
+            />
+          )}
         </div>
       );
     });
-  }
+  };
   return (
     <div className="IndividualCourse">
       <Header />
-      <div className="course_individual_data" >
+      <div className="course_individual_data">
         <img
           src={
             course
@@ -122,71 +179,74 @@ function IndividualCourse() {
           height="auto"
         />
 
-        <h1 style={{ color: "#0d47a1" }}>{course ? course.courseDetails.courseTitle : "Course Title"}</h1>
-        <p style={{ color: '#64b5f6' }}>
+        <h1 style={{ color: "#0d47a1" }}>
+          {course ? course.courseDetails.courseTitle : "Course Title"}
+        </h1>
+        <p style={{ color: "#64b5f6" }}>
           {course
             ? course.courseDetails.courseDescription
             : "Course Description"}
         </p>
         <Toolbar />
 
-        {!admin && course && course.courseDetails.videos && <Box sx={{
-          position: 'relative',
-          display: 'inline-flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-
-          {/* Background Circle (Empty Part) */}
-          <CircularProgress
-            variant="determinate"
-            value={100} // Full circle
-            size={100}
-            thickness={5}
-            sx={{
-              color: 'rgb(157, 157, 250)', // Example custom color for the empty part (light grey)
-              position: 'absolute', // Position it behind the actual progress circle
-            }}
-          />
-          {/* Actual Progress Circle (Filled Part) */}
-          <CircularProgress
-            variant="determinate"
-            value={progress}
-            size={100}
-            thickness={5}
-            sx={{
-              color: 'Blue',
-            }}
-          />
+        {!admin && course && course.courseDetails.videos && (
           <Box
             sx={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              bottom: 0,
-              right: 0,
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
+              position: "relative",
+              display: "inline-flex",
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
-            <Typography
-              variant="caption"
-              component="div"
+            {/* Background Circle (Empty Part) */}
+            <CircularProgress
+              variant="determinate"
+              value={100} // Full circle
+              size={100}
+              thickness={5}
               sx={{
-                color: 'Blue',
-                fontWeight: 'bold',
+                color: "rgb(157, 157, 250)", // Example custom color for the empty part (light grey)
+                position: "absolute", // Position it behind the actual progress circle
+              }}
+            />
+            {/* Actual Progress Circle (Filled Part) */}
+            <CircularProgress
+              variant="determinate"
+              value={progress}
+              size={100}
+              thickness={5}
+              sx={{
+                color: "Blue",
+              }}
+            />
+            <Box
+              sx={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
               }}
             >
-              {`${Math.round(progress)}%`}
-            </Typography>
+              <Typography
+                variant="caption"
+                component="div"
+                sx={{
+                  color: "Blue",
+                  fontWeight: "bold",
+                }}
+              >
+                {`${Math.round(progress)}%`}
+              </Typography>
+            </Box>
           </Box>
-        </Box>
-        }
-        
+        )}
 
-          {course && course.courseDetails.videos ? (
-            <Box
+        {course && course.courseDetails.videos ? (
+          <Box
             component="main"
             sx={{
               flexGrow: 1,
@@ -196,7 +256,6 @@ function IndividualCourse() {
               justifyContent: "left",
             }}
           >
-          
             <Container
               maxWidth="lg"
               sx={{
@@ -206,23 +265,26 @@ function IndividualCourse() {
                 justifyContent: "space-evenly",
                 margin: "auto",
                 marginLeft: 8,
-                marginRight:8
+                marginRight: 8,
               }}
             >
               {courseVideos()}
             </Container>
           </Box>
-            
-          ) : (
-            <>
-              <Typography textAlign={"center"} sx={{ color: '#64b5f6' }}>
-                You Are Not Enrolled Yet
-              </Typography>
-              <Button variant="contained" onClick={enroll} sx={{ width: "20vw", mt: 2 }}>
-                Enroll
-              </Button>
-            </>
-          )}
+        ) : (
+          <>
+            <Typography textAlign={"center"} sx={{ color: "#64b5f6" }}>
+              You Are Not Enrolled Yet
+            </Typography>
+            <Button
+              variant="contained"
+              onClick={enroll}
+              sx={{ width: "20vw", mt: 2 }}
+            >
+              Enroll
+            </Button>
+          </>
+        )}
 
         <Toolbar />
         {course && course.courseDetails.enrolledUsers ? (
@@ -254,5 +316,3 @@ function IndividualCourse() {
 }
 
 export default IndividualCourse;
-
-
